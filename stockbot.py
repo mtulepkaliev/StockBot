ad580b70aa23db88fcc0cf78e6fdf37ff5d9b382
Moses Tulepkaliev <moses1.tule@gmail.com>
2022-03-17 00:06:08 -0600

    Fixed weird string extraction by using nextcord's bot extension, also makes it easier to define commands, this literally made my code 1000x better

--- stockbot.py
+++ stockbot.py
@@ -1,7 +1,9 @@
 
 import os
 import nextcord
+from nextcord.ext import commands
 import yfinance as yf
+import re
 from dotenv import load_dotenv
 
 from yfincode import getPriceOutput
@@ -10,48 +12,27 @@ from yfincode import getPriceOutput
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
+bot=commands.Bot(command_prefix='!stockbot ')
 
+@bot.event
+async def on_ready():
+        print('Logged on !')
+@bot.command()
+async def price(context,arg):
+    print('got inside price')
+    ticker_text = str(arg)
+    print('Ticker Requested:' + ticker_text)
+    try:
+        ticker = yf.Ticker(ticker_text)
+        ticker_info = ticker.info
+        print('Info received on ' + ticker_text)
+        if(str(ticker_info['regularMarketPrice']) == 'None'):
+            raise Exception("INVALID_TICKER_ERROR")
+        else:
+            price = str(ticker_info['regularMarketPrice'])
+    except Exception as e:
+        await context.send(content=e)
+        return
+    await context.send(content=getPriceOutput(ticker_text,ticker_info['shortName'],price,ticker_info['open']))
 
-class MyClient(nextcord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self,message):
-        if(str(message.content).startswith('!stockbot ')):
-            channel = message.channel
-            #extract the part of the messafe after '!stockbot'
-            command_text = str(message.content)[10:len(str(message.content))]
-            command_text = command_text.strip()
-
-            print("Command Received:" + command_text)
-
-            #price command
-            if(command_text.startswith('price ')):
-
-                #extract the ticekr
-                ticker_text = command_text[6:len(command_text)]
-                print('Ticker Requested:' + ticker_text)
-                try:
-                    ticker = yf.Ticker(ticker_text)
-                    ticker_info = ticker.info
-                    print('Info received on ' + ticker_text)
-                    if(str(ticker_info['regularMarketPrice']) == 'None'):
-                        raise Exception("INVALID_TICKER_ERROR")
-                    else:
-                        price = str(ticker_info['regularMarketPrice'])
-                except Exception as e:
-                    await channel.send(content=e)
-                    return
-                
-                await channel.send(content=getPriceOutput(ticker_text,ticker_info['shortName'],price,ticker_info['open']))
-
-
-
-
-
-
-
-            
-
-client = MyClient()
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
