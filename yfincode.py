4a0d83ce56433920e5d6a1d1365a6dc03039d42f
Moses Tulepkaliev <moses1.tule@gmail.com>
2022-03-18 20:45:40 -0600

    Updated output format, added comments and whitespace

--- yfincode.py
+++ yfincode.py
@@ -5,14 +5,21 @@ import json
 from decimal import *
 
 def getPriceOutput(ticker_info):
+
+    #save the part of the dict we need (for readability purposes)
     price_stats = ticker_info['price']
+
     decFormat = Decimal('0.01')
+
+    #retreve needed varaibles
     price = Decimal(price_stats['regularMarketPrice']).quantize(decFormat)
     pricechange = Decimal(price_stats['regularMarketChange']).quantize(decFormat)
     pctchange = Decimal(price_stats['regularMarketChangePercent'] * 100).quantize(decFormat)
+
     pctchange = str(pctchange) + '%'
+
     if (Decimal(pricechange) >= 0):   
         pricechange = '+' + str(pricechange)
         pctchange = '+' + pctchange
-    output = f"{price_stats['shortName']} ({price_stats['symbol']}) {price} {pricechange} {pctchange}"
+    output = f"{price_stats['shortName']} ({price_stats['symbol']}) \nPrice: {price} \nDay Change: {pricechange}\nPercent Change: {pctchange}"
     return output
