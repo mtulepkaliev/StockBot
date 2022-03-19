ab17e2c75310a0e5e04f5f790c3e1d7dcf1c73ee
Moses Tulepkaliev <moses1.tule@gmail.com>
2022-03-18 20:43:47 -0600

    Updated Exception Handling and Cleaned Up Code
    Exception handling was broken from last commit due to no longer using
    ticker.info, also commented code a bit

--- stockbot.py
+++ stockbot.py
@@ -23,16 +23,25 @@ async def price(context,ticker_name):
     print('Ticker Requested:' + ticker_name)
     try:
         ticker = yf.Ticker(ticker_name)
-        print('Info received on ' + ticker_name)
+
         ticker_stats = ticker.stats()
-        #print('info:' + str(ticker_stats))
-        if (str(ticker_stats['price']['regularMarketPrice']) == 'None'):
-            raise Exception("INVALID_TICKER_ERROR")
-        else:
-            await context.send(getPriceOutput(ticker_stats))
+        print('Info received on ' + ticker_name)
+
+        #try accesing the market price, returns KeyError if there is none
+        try:
+            ticker_stats['price']['regularMarketPrice']
+        except KeyError as e:
+            #tell user they entered the wrong ticker
+            print("Exception:" + str(e))
+            await context.send(content='Invalid Ticker, please try again')
+            return
+        #pass ticker_stats to get the output
+        await context.send(getPriceOutput(ticker_stats))
+
+    #print unknown exeception for all other exceptions
     except Exception as e:
-        print("Exception" + str(e))
-        await context.send(content=e)
+        print("Exception:" + str(e))
+        await context.send(content='Unknown Exception Occured, please try again')
         return
 
 bot.run(DISCORD_TOKEN)
